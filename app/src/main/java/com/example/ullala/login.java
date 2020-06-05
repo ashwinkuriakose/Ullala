12bd200d2efec540516d75f8f59bceb3b0fa9173
Ashwin Kuriakose <ashwinkuriakose143@gmail.com>
2020-06-05 20:46:05 +0530

    added login, otp and home page

--- app/src/main/java/com/example/ullala/login.java
+++ app/src/main/java/com/example/ullala/login.java
@@ -4,14 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Intent myIntent = new Intent(getBaseContext(),   otp.class);
-        startActivity(myIntent);
+
+        button=(Button)findViewById(R.id.generate_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(getApplicationContext(),otp.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ullala/otp.java
+++ app/src/main/java/com/example/ullala/otp.java
@@ -4,14 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class otp extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
-        Intent myIntent = new Intent(getBaseContext(),   home.class);
-        startActivity(myIntent);
+
+        button=(Button)findViewById(R.id.verify_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(getApplicationContext(),home.class);
+                startActivity(i);
+            }
+        });
     }
 }
