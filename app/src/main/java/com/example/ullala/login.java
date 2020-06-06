3fe91ba521279062bca0823130f1c612358f46d1
Ashwin Kuriakose <ashwinkuriakose143@gmail.com>
2020-06-06 21:48:39 +0530

    rollbackes sms code

--- app/src/main/java/com/example/ullala/login.java
+++ app/src/main/java/com/example/ullala/login.java
@@ -16,27 +16,15 @@ import android.widget.Toast;
 
 public class login extends AppCompatActivity {
     Button button;
-    Button submit;
-    EditText contact;
-    String phNo;
-    ProgressDialog progress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        contact = (EditText) findViewById(R.id.phone_number_text);
-        submit = (Button) findViewById(R.id.generate_btn);
 
-        submit.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                phNo = contact.getText().toString();
-                new CheckOwnMobileNumber().execute();
-                Toast.makeText(getApplicationContext(), phNo, Toast.LENGTH_LONG).show();
-            }
-        });
-       /* button=(Button)findViewById(R.id.generate_btn);
+        button=(Button)findViewById(R.id.generate_btn);
         button.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -45,57 +33,9 @@ public class login extends AppCompatActivity {
                 Intent i = new Intent(getApplicationContext(),otp.class);
                 startActivity(i);
             }
-        });*/
+        });
     }
 
 
-    private class CheckOwnMobileNumber extends AsyncTask<String, Void, String> {
-        @Override
-        protected void onPostExecute(String result) {
-            // TODO Auto-generated method stub
-            if (progress.isShowing()) {
-                progress.dismiss();
 
-                // Check SMS Received or not after that open dialog date
-                /*if(SMSReceiver.str.equals(phNo))
-                {
-                    Toast.makeText(getApplicationContext(), "Thanks for providing your number.", Toast.LENGTH_LONG).show();
-                }
-                else
-                {
-                    Toast.makeText(getApplicationContext(), "Provide your own mobile number please.", Toast.LENGTH_LONG).show();
-                    return;
-                }*/
-
-            }
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            // TODO Auto-generated method stub
-            String msg = phNo;
-            try {
-                sendSMS(phNo, msg);
-            } catch (Exception ex) {
-                Log.v("Exception :", "" + ex);
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            // TODO Auto-generated method stub
-            progress = ProgressDialog.show(login.this, "", "Checking Mobile Number...");
-            progress.setIndeterminate(true);
-            progress.getWindow().setLayout(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
-            super.onPreExecute();
-        }
-
-        private void sendSMS(String phoneNumber, String message) {
-            //PendingIntent pi = PendingIntent.getActivity(getApplicationContext(), 0, new Intent(getApplicationContext(), MainActivity.class), 0);
-            SmsManager sms = SmsManager.getDefault();
-            sms.sendTextMessage(phoneNumber, null, message, null, null);
-
-        }
-    }
 }
