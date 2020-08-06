fb948366208c57b2a6236532d3d68976541ef167
Ashwin Kuriakose <ashwinkuriakose143@gmail.com>
2020-08-06 23:27:42 +0530

    date picker added and minor allignment

--- app/src/main/java/com/example/ullala/RulesFragment.java
+++ app/src/main/java/com/example/ullala/RulesFragment.java
@@ -1,6 +1,5 @@
 package com.example.ullala;
 
-import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,20 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.DatePicker;
-import android.widget.EditText;
-
-import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link DobFragment#newInstance} factory method to
+ * Use the {@link RulesFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DobFragment extends Fragment {
+public class RulesFragment extends Fragment {
 
-    public EditText date;
-    DatePickerDialog datePickerDialog;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -31,8 +24,7 @@ public class DobFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-
-    public DobFragment() {
+    public RulesFragment() {
         // Required empty public constructor
     }
 
@@ -42,11 +34,11 @@ public class DobFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment DobFragment.
+     * @return A new instance of fragment rules.
      */
     // TODO: Rename and change types and number of parameters
-    public static DobFragment newInstance(String param1, String param2) {
-        DobFragment fragment = new DobFragment();
+    public static RulesFragment newInstance(String param1, String param2) {
+        RulesFragment fragment = new RulesFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,32 +53,12 @@ public class DobFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_dob, container, false);
-        date=(EditText) view.findViewById(R.id.date);
-        date.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final Calendar calendar= Calendar.getInstance();
-                 int year=calendar.get(Calendar.YEAR);
-                 int month=calendar.get(Calendar.MONTH);
-                 int day=calendar.get(Calendar.DAY_OF_MONTH);
-               datePickerDialog= new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
-                   @Override
-                   public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                       date.setText(dayOfMonth+"-"+(monthOfYear+1)+"-"+year);
-                   }
-               },year,month,day);
-               datePickerDialog.show();
-            }
-        });
-        return view;
+        return inflater.inflate(R.layout.fragment_rules, container, false);
     }
 }
